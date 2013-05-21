dff7c3c5f0676f43e89a25cb457d0849d6a3ca55
Mert Susur <mail@mertsusur.com>
2013-05-21 17:12:18 +0300

    nuget package added.

--- GitHook/Properties/AssemblyInfo.cs
+++ GitHook/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("GitHook")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A basic git hook handler for creating tasks when git sends a hook request.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("GitHook")]
